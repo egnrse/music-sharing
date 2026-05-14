5ca969c7c92042a9324688be8fa7f6bdc96f2893
Elia Nitsche <nitscheelia@gmail.com>
2026-05-15 01:27:47 +0200

    fix(public/src/main): temp fix

--- public/src/main.ts
+++ public/src/main.ts
@@ -4,29 +4,31 @@
  * @author Elia
  */
 
-import type { singleFile } from  "./globals.js";
+import type { filesAPI } from "./shared/shared.js"
 import { log } from  "./globals.js";
 import { Track } from "./Track.js";
 import { Table } from "./Table.js";
 import { Player } from "./Player.js";
+//import FrontSong from "./FrontSong.js";
 
 
 /// VAR/CONST
 let table:Table;
 let player:Player;
 
-let fileListRaw:singleFile[] = [];
-let trackList:Track[] = [];
+let fileListRaw:filesAPI = [];
+//let songList:FrontSong[] = [];
+let trackList:Track[] = [];		//dev
 
 
 /// FUNCTIONS
 /**
  *	fetches the file list
  */
-async function fetchFileList(): Promise<singleFile[]> {
+async function fetchFileList(): Promise<filesAPI> {
 	log(`fetch: files.php`, 3);
 	//const data: singleFile[] = await fetch(`list_files.php?offset=${offset}&limit=${limit}`).then(r => r.json());
-	const data: singleFile[] = await fetch(`api/files`).then(r => r.json());
+	const data: filesAPI = await fetch(`api/files`).then(r => r.json());
 	//totalFiles = data.total;
 	return data;
 }
@@ -51,8 +53,13 @@ async function main() {
 	const data = await fetchFileList();
 	fileListRaw.push(...data);
 	for (const f of fileListRaw) {
-		const track = new Track(f.folder, f.name);
+		const dir = f.path.substring(0, f.path.lastIndexOf('/'));	//dev
+		const base = f.path.split('/').pop();	//dev
+		const track = new Track(dir, base);	//dev
 		trackList.push(track);
+
+		//const song = new FrontSong(f.path, f.name, f.artist);
+		//songList.push(song);
 
 		// autoload song
 		if (pathParam && track.path === pathParam) {
